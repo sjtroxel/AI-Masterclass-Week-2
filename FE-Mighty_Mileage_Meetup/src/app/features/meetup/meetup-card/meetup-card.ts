dbccda4ceb725299b7d1affdda52d2c43a5621c3
sjtroxel <sjtroxel@protonmail.com>
2026-02-14 17:33:04 -0600

    complete join/leave functionality with reactive UI updates

--- FE-Mighty_Mileage_Meetup/src/app/features/meetup/meetup-card/meetup-card.ts
+++ FE-Mighty_Mileage_Meetup/src/app/features/meetup/meetup-card/meetup-card.ts
@@ -1,5 +1,6 @@
-import { Component, input, output } from '@angular/core';
+import { Component, computed, inject, input, output } from '@angular/core';
 import { Meetup } from '../../../shared/models/meetup';
+import { MeetupService } from '../../../core/services/meetup';
 import { DatePipe } from '@angular/common';
 
 @Component({
@@ -10,9 +11,33 @@ import { DatePipe } from '@angular/common';
   styleUrl: './meetup-card.scss'
 })
 export class MeetupCardComponent {
+  private meetupService = inject(MeetupService);
+
   meetup = input.required<Meetup>();
   currentUserId = input<number | null>(null);
 
   edit = output<Meetup>();
   delete = output<number>();
+
+  isOwner = computed(() => this.meetup().user?.id === this.currentUserId());
+
+  isParticipant = computed(() => {
+    const userId = this.currentUserId();
+    const participants = this.meetup().meetup_participants;
+    if (!userId || !participants) return false;
+    return participants.some((p) => p.user_id === userId);
+  });
+
+  participantCount = computed(() => this.meetup().meetup_participants?.length ?? 0);
+
+  joinMeetup() {
+    this.meetupService.joinMeetup(this.meetup().id);
+  }
+
+  leaveMeetup() {
+    const userId = this.currentUserId();
+    if (userId) {
+      this.meetupService.leaveMeetup(this.meetup().id, userId);
+    }
+  }
 }
